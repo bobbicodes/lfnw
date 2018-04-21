042b0892f43e2885850ddea35a79d3d0fd6682de
Bobby Towers <porkostomus@mail.com>
2018-04-20 18:39:13 -0700

    Create clojure.sh

---
+++ clojure.sh
@@ -0,0 +1,7 @@
+# Install Clojure CLI Tools. More info: https://clojure.org/guides/getting_started
+
+curl -O https://download.clojure.org/install/linux-install-1.9.0.375.sh
+chmod +x linux-install-1.9.0.375.sh
+sudo ./linux-install-1.9.0.375.sh
+
+  
